0d9adb504bf8bc6ae0432275bcae319aed43a867
Fernando Ortis Saldaña <ortissaldana@Fernandos-MacBook-Pro.local>
2023-06-24 08:12:36 -0600

    m

--- src/main/java/com/example/demo/web/controller/PaymentController.java
+++ src/main/java/com/example/demo/web/controller/PaymentController.java
@@ -66,7 +66,7 @@ public class PaymentController {
             PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
 
                     .setCurrency("mxn") // Reemplaza con la moneda deseada
-                    .setAmount(40000000L) // Reemplaza con el monto deseado
+                    .setAmount(100L) // Reemplaza con el monto deseado
                     .setDescription("Pago por transferencia a DriveAI")
                     .setCustomer("cus_NrqZuXNiuXF4Ph")
                     .setReceiptEmail("ortissaldana@icloud.com")
